e529fbeb3bb86b452c97e390576b65f1e1105ba9
Alex <reyesruizalex@gmail.com>
2019-10-27 11:06:41 -0400

    db

--- API.py
+++ API.py
@@ -1,19 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
+import os
+import psycopg2
+import sqlite3
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello(): 
-    return jsonify({"about": "Hello World!"})
+app.database = "sample.db"
 
 @app.route("/bookList")
 def bookList(): 
-    file = open('bookList.txt', 'r')
-    lines = file.read().split("\n")
-    
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    print(posts)
+
+    # file = open('bookList.txt', 'r')
+    # lines = file.read().split("\n")
+    lines = posts
+
     response = jsonify(lines)
     response.headers.add('Access-Control-Allow-Origin', '*')
     
     file.close()
-    
+
     return response
+
+def connect_db(): 
+    return sqlite3.connect(app.database)
