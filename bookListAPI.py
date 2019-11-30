f12499940002cd01147d3b87b1b5365439aa14bc
Alex <reyesruizalex@gmail.com>
2019-11-30 17:23:53 -0500

    a

--- bookListAPI.py
+++ bookListAPI.py
@@ -64,20 +64,20 @@ def add_book():
 @app.route("/book2019", methods=["GET"])
 def get_book():
     all_books = Book.query.all()
-    print("incoming")
-
-    print("incoming")
-
-    print("incoming")
-
-    print("incoming")
-    print(all_books)
-    print("incoming")
-
-    print("incoming")
-    print("incoming")
-
     result = books_schema.dump(all_books)
+    print("incoming")
+
+    print("incoming")
+
+    print("incoming")
+
+    print("incoming")
+    print(result)
+    print("incoming")
+
+    print("incoming")
+    print("incoming")
+
     return jsonify(result)
 
 
