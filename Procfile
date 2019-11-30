132deccf46341de3e3b456f182f9b96e184f21ed
Alex <reyesruizalex@gmail.com>
2019-11-29 21:22:05 -0500

    added wsgi

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn bookListAPI:app
+web: gunicorn wsgi:app
---
+++ wsgi.py
@@ -0,0 +1,16 @@
+from flask import Flask 
+
+from commands import create_tables
+from extensions import db
+from models import Book
+
+def create_app(config_file='settings.py'):
+    app = Flask(__name__)
+
+    app.config.from_pyfile(config_file)
+
+    db.init_app(app)
+
+    app.cli.add_command(create_tables)
+
+    return app
